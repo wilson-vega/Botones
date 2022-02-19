2e316caf52c49e71b7486b87b9f07d977dd6c69b
wilson-vega <wilsondvz39@gmail.com>
2022-02-18 21:52:40 -0500

    botones if

---
+++ botonos.js
@@ -0,0 +1,142 @@
+
+
+let digitado = document.getElementById("digitado");
+let teclaA = document.getElementById("teclaA");
+let teclaB = document.getElementById("teclaB");
+let teclaC = document.getElementById("teclaC");
+let teclaD = document.getElementById("teclaD");
+let teclaE = document.getElementById("teclaE");
+let teclaF = document.getElementById("teclaF");
+let teclaG = document.getElementById("teclaG");
+let teclaH = document.getElementById("teclaH");
+let teclaI = document.getElementById("teclaI");
+let teclaJ = document.getElementById("teclaJ");
+let teclaK = document.getElementById("teclaK");
+let teclaL = document.getElementById("teclaL");
+
+
+
+
+
+
+let a = () => {
+    digitado.innerText = 1
+};
+
+teclaA.onclick = function () {
+    a();
+}
+
+/*
+function mostrarb() {
+    digitado.innerText = 2
+}
+*/
+
+let b = () => {
+    digitado.textContent = 2
+};
+
+teclaB.onclick = function () {
+    b();
+}
+
+
+
+let c = () => {
+    digitado.innerText = 3
+};
+
+teclaC.onclick = function () {
+    c();
+}
+
+
+
+let d = () => {
+    digitado.innerText = 4
+};
+
+teclaD.onclick = function () {
+    d();
+}
+
+
+
+let e = () => {
+    digitado.innerText = 5
+};
+
+teclaE.onclick = function () {
+    e();
+}
+
+
+
+let f = () => {
+    digitado.innerText = 6
+};
+
+teclaF.onclick = function () {
+    f();
+}
+
+
+
+let g = () => {
+    digitado.innerText = 7
+};
+
+teclaG.onclick = function () {
+    g();
+}
+
+
+
+let h = () => {
+    digitado.innerText = 8
+};
+
+teclaH.onclick = function () {
+    h();
+}
+
+
+let i = () => {
+    digitado.innerText = 9
+};
+
+teclaI.onclick = function () {
+    i();
+}
+
+
+let j = () => {
+    digitado.innerText = 10
+};
+
+teclaJ.onclick = function () {
+    j();
+}
+
+
+let k = () => {
+    digitado.innerText = 11
+};
+
+teclaK.onclick = function () {
+    k();
+}
+
+
+
+let l = () => {
+    digitado.innerText = 12
+};
+
+teclaL.onclick = function () {
+    l();
+}
+
+
+
